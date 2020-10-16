6c70ce67a1ba9ebb260ee763df75fd1afccab8d8
= <harrison.lindgren@gmail.com>
2020-10-16 16:00:34 -0400

    The on/off button now updates its text upon being clicked

--- static/index.html.js
+++ static/index.html.js
@@ -1,7 +1,10 @@
 let isClockOn = true;
 
+let clockElement = document.getElementById('index-clock-container');
+let clockTextElement = document.getElementById('index-clock-text');
+
 //sends a request to the server that will either turn the clock on or off
 function toggleClockOnOff() {
     isClockOn = !isClockOn;
-    console.log('DEBUG: Turning clock ' + String((isClockOn) ? 'on' : 'off'));
+    clockTextElement.innerHTML = String((isClockOn) ? 'ON' : 'OFF');
 }
